4d633eb515e4d7d09fe9e3b064020c808494a1d3
Cha <chalaalemayehu1824@gmail.com>
2026-04-01 00:43:33 +0300

    starting chat features

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ dotenv.config({ path: './config.env' });
 // -------------------- Connect to Remote Hosted DataBase ---------------------- //
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-// mongoose.connect(DB, {
-//   useNewUrlParser: true,
-// }).then(() => console.log('Remote database connection successfull!')).catch(err => {
-//   console.log("ERROR: " + err);
-// });
-
-mongoose.connect(process.env.DATABASE_LOCAL, {
-}).then(() => console.log('Local database connection successfull!')).catch(err => {
-    console.log("ERROR: " + err);
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+}).then(() => console.log('Remote database connection successfull!')).catch(err => {
+  console.log("ERROR: " + err);
 });
+
+// mongoose.connect(process.env.DATABASE_LOCAL, {
+// }).then(() => console.log('Local database connection successfull!')).catch(err => {
+//     console.log("ERROR: " + err);
+// });
 
 // Create HTTP server and attach Socket.io
 const server = http.createServer(app);
